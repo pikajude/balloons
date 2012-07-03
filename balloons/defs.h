c243ce558e4493dc6b8c2143654f12b5471b8919
Joel <barebonesgraphics@gmail.com>
2012-07-02 20:37:35 -0400

    clean up spacing

--- balloons/defs.h
+++ balloons/defs.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define BVERSION "0.0"
-#define BINIT_FUNCTION "balloons_init"
-#define BKEY_USERNAME "_username"
-#define BKEY_OAUTHCODE "_oauthtoken"
-#define BKEY_OAUTHRTOKEN "_oauthrtoken"
-#define BKEY_TRIGGER "_trigger"
+#define BVERSION            "0.0"
+#define BINIT_FUNCTION      "balloons_init"
+#define BKEY_USERNAME       "_username"
+#define BKEY_OAUTHCODE      "_oauthtoken"
+#define BKEY_OAUTHRTOKEN    "_oauthrtoken"
+#define BKEY_TRIGGER        "_trigger"
 #define BKEY_EXTENSIONS_DIR "_extdir"
 
 #define zero(a,b) memset(a,0,b)
