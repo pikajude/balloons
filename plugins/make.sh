8fbb894fb46909e3a53c342cf91ca17ca5c9c5e9
Joel <barebonesgraphics@gmail.com>
2012-07-12 21:33:51 -0400

    allow debug switch

--- plugins/make.sh
+++ plugins/make.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+debug=
+while getopts ":d" opt; do
+    case $opt in
+        d)
+            debug=g
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 if [[ -z $1 ]]; then
     echo "Usage: sh make.sh name_of_library"
     exit
 fi
-gcc -Wall -fPIC -cg "$1.c" -I../balloons 
+gcc -Wall -fPIC "-c$debug" "$1.c" -I../balloons 
 gcc -shared "-Wl,-install_name,$1.so" -o "$1.so" "$1.o" ../build/*.o -lcurl
